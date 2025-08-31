ca4466997f693d223f7856d42c63831a0a36ff75
Oliwier <worekit@gmail.com>
2025-08-31 19:52:42 +0200

    matches schedule style, with scroll in it and sidebar teams

---
+++ frontend/src/components/matches/Matches.tsx
@@ -0,0 +1,18 @@
+import "./Matches.css";
+
+export const Matches = () => {
+  return (
+    <div className="matches__container">
+      <div className="match__date">14 May 2025</div>
+      <div className="match__info"></div>
+      <div className="match__date">16 May 2025</div>
+      <div className="match__info"></div>
+      <div className="match__info"></div>
+      <div className="match__date">18 May 2025</div>
+      <div className="match__info"></div>
+      <div className="match__info"></div>
+      <div className="match__info"></div>
+      <div className="match__info"></div>
+    </div>
+  );
+};
--- frontend/src/components/schedule/Schedule.tsx
+++ frontend/src/components/schedule/Schedule.tsx
@@ -1,12 +1,14 @@
 import "./Schedule.css";
 import { NavigateBar } from "../navigate/NavigateBar";
 import { Searchbar } from "../searchbar/Searchbar";
+import { Matches } from "../matches/Matches";
 
 export const Schedule = () => {
   return (
     <div className="schedule__container">
       <Searchbar />
       <NavigateBar />
+      <Matches />
     </div>
   );
 };
