b2d25542faa2c18b4101d7100b8e2bd72bde71ef
Oliwier <worekit@gmail.com>
2025-09-11 16:36:00 +0200

    team bg logo with poacity and transition

--- frontend/src/components/sidebar/Team.tsx
+++ frontend/src/components/sidebar/Team.tsx
@@ -12,13 +12,7 @@ export const Team = ({ onlyLogo, logo, name }: TeamProps) => {
   };
 
   return (
-    <div
-      className={`following__team ${onlyLogo ? "" : "only-logo"}`}
-      style={{
-        background: `#1f1f1f url(${logo}) no-repeat center`,
-        backgroundSize: "55%",
-      }}
-    >
+    <div className={`following__team ${onlyLogo ? "" : "only-logo"}`} style={{ ["--team-logo" as string]: `url(${logo})` }}>
       <div className="following__team__name">{name}</div>
       <div className="following__team__unfollow" onClick={handleUnfollow}></div>
     </div>
