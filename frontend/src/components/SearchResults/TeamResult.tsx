cf062f47dca0883dd999529a4aafb23ad4ab63da
Oliwier Pasko <worekit@gmail.com>
2025-09-16 15:27:55 +0200

    fix search result teams rank and unused function

--- frontend/src/components/SearchResults/TeamResult.tsx
+++ frontend/src/components/SearchResults/TeamResult.tsx
@@ -10,16 +10,19 @@ type TeamResultProps = {
 export const TeamResult = ({ rank, logo, name, slug }: TeamResultProps) => {
   const handleFollowTeam = () => {
     //TODO: Add team
+    console.log(`Follow team: ${name} (${slug})`);
   };
 
   return (
     <div className={`team__result`}>
-      <div className={`team__result__rank`}>#{rank}</div>
+      <div className={`team__result__rank`}>{rank ? `#${rank}` : `-`}</div>
       <div className={`team__result__logo`}>
         <img src={logo} alt="logo" />
       </div>
       <div className={`team__result__name`}>{name}</div>
-      <div className={`team__result__follow`}>+</div>
+      <div className={`team__result__follow`} onClick={handleFollowTeam}>
+        +
+      </div>
     </div>
   );
 };
